1948f9706395526fe4b8886ab2d100f3c4291e32
Thaveesha Sanjana <thaveeshasanjana4@gmail.com>
2025-03-18 12:52:12 +0530

    Add pages to Nav

--- Frontend/src/components/Navbar3.js
+++ Frontend/src/components/Navbar3.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import AddProducts from '../pages/Product/AddProducts';
+import ManageProducts from '../pages/Product/ManageProducts';
 
 const Navbar3 = () => {
   return (
     <nav className="navbar navbar-expand-lg" style={{ backgroundColor: 'rgb(0, 78, 156)' }}>
       <div className="container-fluid d-flex justify-content-center">
-        <Link to="/add-products" className="nav-link text-white mx-3">Add Products</Link>
-        <Link to="/manage-products" className="nav-link text-white mx-3">Manage Products</Link>
+        <Link to="/add-products" className="nav-link text-white mx-3">{<AddProducts />}</Link>
+        <Link to="/manage-products" className="nav-link text-white mx-3">{<ManageProducts />}</Link>
       </div>
     </nav>
   );
