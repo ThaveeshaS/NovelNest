c1e4a5db9f7365c9bb5c24066ac8e09d2547c9ee
Thaveesha Sanjana <thaveeshasanjana4@gmail.com>
2025-03-30 11:42:01 +0530

    Update AddtoCart icon

--- Frontend/src/components/Navbar2.js
+++ Frontend/src/components/Navbar2.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useWishlist } from "../pages/Product/WishlistContext";
+import { useCart } from "../pages/Product/CartContext";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Navbar2 = () => {
   const { wishlist } = useWishlist(); // Get wishlist from context
+  const { cartItems } = useCart();   // Get cart items from context
 
   return (
     <nav className="navbar navbar-expand-lg" style={{ backgroundColor: 'rgb(0, 78, 156)' }}>
@@ -56,8 +58,8 @@ const Navbar2 = () => {
             )}
           </Link>
 
-          {/* Add to Cart Icon */}
-          <Link to="/cart" className="nav-link text-white mx-3">
+          {/* Add to Cart Icon with Count */}
+          <Link to="/cart" className="nav-link text-white mx-3 position-relative">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -73,6 +75,21 @@ const Navbar2 = () => {
               <line x1="3" y1="6" x2="21" y2="6"></line>
               <path d="M16 10a4 4 0 0 1-8 0"></path>
             </svg>
+            {cartItems.length > 0 && (
+              <span 
+                className="position-absolute top-0 start-100 translate-middle badge rounded-pill"
+                style={{ 
+                  backgroundColor: 'rgba(255, 255, 255, 0.9)',
+                  color: 'rgb(0, 78, 156)',
+                  fontSize: '0.7rem',
+                  fontWeight: 'bold',
+                  padding: '2px 6px',
+                  border: '1px solid rgb(0, 78, 156)'
+                }}
+              >
+                {cartItems.length}
+              </span>
+            )}
           </Link>
         </div>
       </div>
